b008adb70293c92cf224fa0091c2447e068f311b
Jordan Mck <ghostdagunna@gmail.com>
2024-02-21 14:46:10 -0600

    Initial Commit

---
+++ NBAPicks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBAPicks
+//
+//  Created by Jordan Mck on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBAPicks/NBAPicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAPicksApp.swift
+//  NBAPicks
+//
+//  Created by Jordan Mck on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NBAPicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
